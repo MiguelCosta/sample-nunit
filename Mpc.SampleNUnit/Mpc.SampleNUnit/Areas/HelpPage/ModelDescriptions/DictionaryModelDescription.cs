0b1fc333e93086572de4aeffad0a2750f034ab1f
Miguel Costa <miguelpintodacosta@gmail.com>
2016-12-01 23:43:03 +0000

    Add project

---
+++ Mpc.SampleNUnit/Mpc.SampleNUnit/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Mpc.SampleNUnit.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Mpc.SampleNUnit/Mpc.SampleNUnit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mpc.SampleNUnit.WebApiApplication" Language="C#" %>
